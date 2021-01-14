910f857a00b23ab25e05d62258310795419f9199
Darshan-pandurangaiah <darshan_pandurangaiah@hotmail.com>
2021-01-14 08:56:55 +0530

    1 file modified, 11 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { WelcomeComponent } from './welcome/welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { BuyersComponent } from './buyers/buyers.component';
+import { SellersComponent } from './sellers/sellers.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, WelcomeComponent ],
+  declarations: [ AppComponent, HelloComponent, WelcomeComponent, LoginComponent, ProductsComponent, BuyersComponent, SellersComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
