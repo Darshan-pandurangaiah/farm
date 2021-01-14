71dc5c4bc3becde4dfe3ce4d5612633d4e522610
Darshan-pandurangaiah <darshan_pandurangaiah@hotmail.com>
2021-01-14 09:23:24 +0530

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { LoginComponent } from './login/login.component';
-import { ProductsComponent } from './products/products.component';
-import { BuyersComponent } from './buyers/buyers.component';
-import { SellersComponent } from './sellers/sellers.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { LoginComponent } from "./login/login.component";
+import { ProductsComponent } from "./products/products.component";
+import { BuyersComponent } from "./buyers/buyers.component";
+import { SellersComponent } from "./sellers/sellers.component";
+import { ApproutingModule } from "./approuting.module";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, WelcomeComponent, LoginComponent, ProductsComponent, BuyersComponent, SellersComponent ],
-  bootstrap:    [ LoginComponent ]
+  imports: [BrowserModule, FormsModule, ApproutingModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    WelcomeComponent,
+    LoginComponent,
+    ProductsComponent,
+    BuyersComponent,
+    SellersComponent
+  ],
+  bootstrap: [LoginComponent]
 })
-export class AppModule { }
+export class AppModule {}
